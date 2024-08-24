a88e65dded3db6fceb873b15028ebf6a1a24defe
Josh Lubawy <jlubawy@gmail.com>
2024-08-24 10:41:33 -0700

    Temp fix for MacOS

--- src/Marshal.c
+++ src/Marshal.c
@@ -1439,9 +1439,9 @@ TPMS_ENC_SCHEME_OAEP_Marshal(TPMS_ENC_SCHEME_OAEP *source, BYTE **buffer, INT32 
 UINT16
 TPMS_ENC_SCHEME_RSAES_Marshal(TPMS_ENC_SCHEME_RSAES *source, BYTE **buffer, INT32 *size)
 {
-    source = source;
-    buffer = buffer;
-    size = size;
+    (void)source;
+    (void)buffer;
+    (void)size;
     return 0;
 }
 
